67e96e0de8c6fa63bdff8539ad4d045bc2bbb493
Janista Sihirunwong <janista.shrw@gmail.com>
2022-05-09 12:37:43 +0700

    done10.2

---
+++ day10/homework10.1/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "antd/dist/antd.css";
+import LayoutComp from "./Components/LayoutComp";
+import TableComp from "./Components/Table";
+
+function App() {
+  return (
+    <div className="App">
+      <LayoutComp />
+    </div>
+  );
+}
+
+export default App;
---
+++ day10/homework10.1/src/Components/LayoutComp.js
@@ -0,0 +1,41 @@
+import { Layout, Menu, Breadcrumb } from "antd";
+import "./layoutComp.css";
+import TableComp from "./Table";
+
+const { Header, Content, Footer } = Layout;
+
+const LayoutComp = () => {
+  return (
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={new Array(15).fill(null).map((_, index) => {
+            const key = index + 1;
+            return {
+              key,
+              label: `nav ${key}`,
+            };
+          })}
+        />
+      </Header>
+      <Content style={{ padding: "0 50px" }}>
+        <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-content">Content</div>
+        <TableComp />
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
+  );
+};
+
+export default LayoutComp;
